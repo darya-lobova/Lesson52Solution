da41d392d2decced56aacda838d116d400ef5cd1
st <st@A510PC3>
2026-04-01 18:27:50 +0300

    Task03 solved

---
+++ Task03/logic.cpp
@@ -0,0 +1,6 @@
+#include "logic.h"
+
+int get_max_element(int array[], int size){
+
+	return;
+}
---
+++ Task03/main.cpp
@@ -0,0 +1,13 @@
+#include "logic.h"
+
+int main() {
+
+	cout << "Max digit: " << << endl;
+	cout << "Max digit: " << << endl;
+	cout << "Max digit: " << << endl;
+	cout << "Max digit: " << << endl;
+	cout << "Max digit: " << << endl;
+	cout << "Max digit: " << << endl;
+
+	return 0;
+}
